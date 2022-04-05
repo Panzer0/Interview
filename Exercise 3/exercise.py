a0a1bac0bd027ae0cfe0d9c2a916b1bd39762584
Panzer0 <daniel.pazur@gmail.com>
2022-04-05 17:26:31 +0200

    Finished exercise 3

--- Exercise 3/exercise.py
+++ Exercise 3/exercise.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
+DAY_COUNT = 80
+FIRST_CYCLE = 6
+USUAL_CYCLE = 8
+CYCLE_FINISHED = 0
+
+
+def progress_day(data):
+    data += [USUAL_CYCLE + 1 for _ in range(data.count(CYCLE_FINISHED))]
+    return [FIRST_CYCLE if x == CYCLE_FINISHED else x-1 for x in data]
+
 
 def lanternfish(filename):
-    # TODO
-    return 0
+    with open(filename, 'r') as file:
+        data = [int(x) for x in file.read().split(',')]
+        for _ in range(DAY_COUNT):
+            data = progress_day(data)
+    return len(data)
 
 
 def main():
@@ -16,7 +29,7 @@ def main():
             print(f"Correct result for case {i + 1}: {result}")
         else:
             print(f"Incorrect result for case {i + 1}: {expected} is " \
-                f"expected, but {result} is returned")
+                  f"expected, but {result} is returned")
 
 
 if __name__ == "__main__":
